84b876f5448bd5fb282662fb7994ac1c83c6a495
Jeremy <jeremy.turinetti@gmail.com>
2016-07-23 12:18:33 -0500

    added NUnit and Moq for tests; added first test class and stub methods

---
+++ LinkChanger.Tests/UrlGeneratorTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace LinkChanger.Tests
+{
+    [TestFixture]
+    public class UrlGeneratorTests
+    {
+        
+        [Test]
+        public void UrlGenerator_GenerateUrl_NullArgument()
+        {
+            // TODO
+        }
+
+        [Test]
+        public void UrlGenerator_GenerateUrl_ValidArgument()
+        {
+            // TODO
+        }
+    }
+}
