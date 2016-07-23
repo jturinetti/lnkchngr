b23ed5e22c0cd02cfeadc61e4ad816f473e70d8a
Jeremy <jeremy.turinetti@gmail.com>
2016-07-23 09:36:49 -0500

    added stubs for services

---
+++ LinkChanger/Services/DefaultUrlGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LinkChanger.Services.Interfaces;
+
+namespace LinkChanger.Services
+{
+    public class DefaultUrlGenerator : IUrlGenerator
+    {
+        public DefaultUrlGenerator()
+        {
+            // TODO
+        }
+
+        public Uri GenerateUrl()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LinkChanger/Services/HashUrlGenerationStrategy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LinkChanger.Services.Interfaces;
+
+namespace LinkChanger.Services
+{
+    public class HashUrlGenerationStrategy : IUrlGenerationStrategy
+    {
+        public HashUrlGenerationStrategy()
+        {
+            // TODO
+        }
+
+        public string GenerateUniqueUrlMap(Uri uri)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
