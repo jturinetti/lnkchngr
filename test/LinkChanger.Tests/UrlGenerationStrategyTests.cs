6b2328b20dc9eb08f5993c49e6c82bb6955db5f8
Jeremy <jeremy.turinetti@gmail.com>
2016-07-24 15:20:55 -0500

    first tests!

--- test/LinkChanger.Tests/UrlGenerationStrategyTests.cs
+++ test/LinkChanger.Tests/UrlGenerationStrategyTests.cs
@@ -2,11 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LinkChanger.Services;
+using LinkChanger.Services.Interfaces;
+using Xunit;
 
 namespace LinkChanger.Tests
 {
     public class UrlGenerationStrategyTests
     {
-        // TODO
+        [Fact]
+        public void HashUrlGenerationStrategy_NullArgument()
+        {
+            IUrlGenerationStrategy strategy = new HashUrlGenerationStrategy();
+            Assert.Throws<ArgumentNullException>(() => strategy.GenerateUniqueUrlMap(null));
+        }
+
+        [Theory]
+        [InlineData("http://www.google.com")]
+        [InlineData("http://www.reddit.com/r/all")]
+        [InlineData("https://www.facebook.com/")]
+        public void HashUrlGenerationStrategy_ValidUrl(string url)  
+        {
+            var uri = new Uri(url);
+            var hash = uri.AbsoluteUri.GetHashCode();
+
+            IUrlGenerationStrategy strategy = new HashUrlGenerationStrategy();            
+
+            var result = strategy.GenerateUniqueUrlMap(uri);
+
+            Assert.NotNull(result);
+            Assert.Equal(hash, result.HashCode);
+            Assert.Equal(uri.AbsoluteUri, result.InputUrl.AbsoluteUri);
+            Assert.EndsWith(hash.ToString(), result.MappedUrlSuffix);
+        }
     }
 }
